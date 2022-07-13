4e1b8e86df919c65524031dfed3bde039aaad92e
hal-alex <102769562+hal-alex@users.noreply.github.com>
2022-07-14 00:16:30 +0100

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,144 @@
+
+function init() {
+    
+    //Elements
+    let startGameBut = document.querySelector(".start")
+    let scoreSpan = document.querySelector(".score")
+    let livesSpan = document.querySelector(".lives")
+    let divOne = document.getElementById("1")
+    console.log(divOne)
+    let testingClass = document.getElementsByClassName("testingclass")
+    console.log(divOne.hasChildNodes())
+    let buttonEl = document.createElement("button")
+    buttonEl.classList.add("moleButton")
+    buttonEl.innerHTML = "Hello"
+    let livesRemaining = 3
+    let previousLocation = 15
+    let score = 0
+   
+    //Execution
+
+    function randomIDGenerator () {
+        let randomNum = Math.floor(Math.random() * 24 + 1)
+        if (randomNum === previousLocation) {
+            console.log("matches")
+            randomIDGenerator()
+        }
+        return randomNum
+    }
+
+    function moveLocation () {
+        divOne = document.getElementById(`${randomIDGenerator()}`)
+        divOne.appendChild(buttonEl)
+    }
+   
+    function beginGame() {
+        divOne = document.getElementById(`${randomIDGenerator()}`)
+        divOne.appendChild(buttonEl)
+        // while (livesRemaining > 0) {
+        //     setTimeout(() => {
+        //         moveLocation
+        //     }, 2000); }
+
+            
+        // }
+        // setInterval(() => {
+        //     moveLocation ()
+        //     // Check if divOne has a child element, if so, remove life 
+        //     if (divOne.hasChildNodes()) {
+        //         console.log("hello if statement")
+        //         livesRemaining -= 1
+        //         livesSpan.innerHTML = livesRemaining
+        //     }
+        // }, timer)
+    }
+
+    function buttonClicked () {
+        score += 100
+        console.log(score)
+        scoreSpan.innerHTML = score
+        divOne.innerHTML = ""
+    }
+
+    //Events
+
+    buttonEl.addEventListener("click", buttonClicked)
+    startGameBut.addEventListener("click", beginGame)
+
+    livesSpan.innerHTML = livesRemaining
+    scoreSpan.innerHTML = score
+}
+
+document.addEventListener('DOMContentLoaded', init)
+
+
+// function init() {
+    
+//     //Elements
+//     let startGameBut = document.querySelector(".start")
+//     let scoreSpan = document.querySelector(".score")
+//     let livesSpan = document.querySelector(".lives")
+//     let divOne = document.getElementById("1")
+//     let buttonEl = document.createElement("button")
+//     buttonEl.classList.add("moleButton")
+//     buttonEl.innerHTML = "Hello"
+//     let livesRemaining = 3
+//     let previousLocation = 4
+//     let currentLocation = 5
+//     let score = 0
+   
+//     //Execution
+
+//     function randomIDGenerator () {
+//         let currentLocation = Math.floor(Math.random() * 24 + 1)
+//         if (currentLocation === previousLocation) {
+//             randomIDGenerator()
+//         }
+//         currentLocation = previousLocation
+//     }
+
+//     function moveLocation () {
+//         randomIDGenerator()
+//         divOne = document.getElementById(`${currentLocation}`)
+//         divOne.appendChild(buttonEl)
+//         // buttonEl.addEventListener("click", moveLocation)
+//     }
+   
+//     function beginGame() {
+//         livesRemaining = 3
+//         score = 0
+//         randomIDGenerator()
+//         divOne = document.getElementById(`${currentLocation}`)
+//         divOne.appendChild(buttonEl)
+//         let timer = 2000
+//         setInterval(() => {
+//             moveLocation ()
+//             // Check if divOne has a child element, if so, remove life 
+//             // if (divOne.contains("button")) {
+//             //     console.log("hello if statement")
+//             //     livesRemaining -= 1
+//             //     livesSpan.innerHTML = livesRemaining
+//             // }
+//         }, timer)
+//     }
+
+//     function buttonClicked () {
+//         score += 100
+//         console.log(score)
+//         scoreSpan.innerHTML = score
+//         divOne.innerHTML = ""
+//     }
+
+//     //Events
+
+//     buttonEl.addEventListener("click", buttonClicked)
+//     startGameBut.addEventListener("click", beginGame)
+
+//     livesSpan.innerHTML = livesRemaining
+//     scoreSpan.innerHTML = score
+// }
+
+// document.addEventListener('DOMContentLoaded', init)
+
+
+//// Old code - changing random generator
